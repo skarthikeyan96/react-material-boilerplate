e21942a477b5c3bb0e1d81a04e688bdd6ff51a4e
karthikeyan <karthikeyan@bangthetable.com>
2021-07-24 10:32:39 +0530

    fix: removing the files after the dependecies are installed

--- bin/create-app.js
+++ bin/create-app.js
@@ -4,6 +4,7 @@
 
 
 const {execSync} = require('child_process')
+const fs = require("fs")
 
 const runCommand = command => {
     try {
@@ -15,8 +16,23 @@ const runCommand = command => {
     return true
 }
 
-
 const repoName = process.argv[2];
+
+if(!repoName)
+{
+    // exit the process with error message
+    console.error('Please add project name')
+    process.exit('-1')
+}
+
+try {
+  if (fs.existsSync(repoName)) {
+    console.log("project directory already exists. please enter new project name")
+  } 
+} catch(e) {
+  console.log("An error occurred.", e)
+}
+
 const gitCheckoutCommand = `git clone https://github.com/skarthikeyan96/react-material-boilerplate ${repoName}`
 const installDepsCommand = `cd ${repoName} && yarn install`
 
@@ -29,6 +45,17 @@ if(!checkedOut) process.exit(-1)
 console.log("Installing the dependencies")
 const installedDeps = runCommand(installDepsCommand)
 
-if(!installDepsCommand) process.exit(-1)
+if(!installedDeps) process.exit(-1)
+
+/**
+ * Delete the setup file and readme 
+ */
+unlinkSync(join(process.cwd(), "README.md"));
+unlinkSync(join(process.cwd(), "bin", "setup.js"));
+rmdirSync(join(process.cwd(), "bin"));
+
+runCommand(`git init && git add . && git commit -am "inital commit"`);
+console.log(`new git repo initialized successfully!`);
+
 
 console.log('Congratulations, You are ready')
